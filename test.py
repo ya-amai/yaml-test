ec81119765678a42e71a9a16f097d4e46f80afda
aina <aina@test.local>
2016-06-16 16:07:35 +0000

    Impl. dup

--- test.py
+++ test.py
@@ -1,35 +1,56 @@
 import yaml
+from copy import deepcopy
 from jq import jq
 from pprint import pprint as pp
 
 
 # datas
-DATA = [
-   {"key": "value"},
-   100
-]
+DATA = { "a": {"key": "value"},
+         "b": 100
+}
 
 EXAMPLES = [
     # simple include to root
     {"data": """
-_include: {"from": "$.[0]"}
+_include: {"from": "$.a"}
      """,
      "result": {"key": "value"}
     },
+    {"data": """
+_include: {"from": "$.a"}
+other: 100
+     """,
+     "result": {"key": "value", "other": 100}
+    },
+
     # simple include to object
     {"data": """
 object:
-  _include: {"from": "$.[0]"}
+  _include: {"from": "$.a"}
      """,
      "result": {"object": {"key": "value"}}
+    },
+    {"data": """
+object:
+  other: "bbb"
+  _include: {"from": "$.a"}
+     """,
+     "result": {"object": {"key": "value", "other": "bbb"}}
     },
 
     # simple duplicate(loop)
     {"data": """
-$object_item: 100
+$"object_\\(.item[1])": 100
 _dup: {"with": [1,2]}
     """,
     "result": {"object_1": 100, "object_2": 100}
+    },
+    {"data": """
+other: ["a"]
+$"object_\\(.item[1])": 100
+_dup: {"with": [1,2]}
+    """,
+    "result": {"other": ["a"], "object_1": 100, "object_2": 100}
     },
 
     # simple exists(if)
@@ -41,52 +62,98 @@ _exsists: {"when": False}
     },
 ]
 
-def _process_include(data, context):
+def _jq(s, c):
+    print("+++:", s, c)
+    if isinstance(s, str) and s.startswith("$"):
+        print("+++: jqin", s[1:])
+        ret = jq(s[1:]).transform(c)
+    else:
+        ret = s
+    return ret
+
+def _process_include(data, context, out):
     # check data has include key
     if "_include" not in data:
-        return
+        return out
 
     # process jq value
-    value = include["from"]
+    value = data["_include"]["from"]
     if value.startswith("$"):
         ret = jq(value[1:]).transform(context)
     else:
         ret = value
 
-    return ret
+    # store result
+    # import pdb; pdb.set_trace()
+    out.update(ret)
+    data.pop("_include")
+
+    return out
+
+def _process_dup(data, context, out):
+    # check data has include key
+    if "_dup" not in data:
+        return out
+
+    # process jq value
+    # import pdb; pdb.set_trace()
+    value = data["_dup"]["with"]
+    if isinstance(value, str) and value.startswith("$"):
+        ret = jq(value[1:]).transform(context)
+    else:
+        ret = value
+    
+    # make deepcopy for duplicate object
+    obj = deepcopy(data)
+
+    # loop
+    for elm in enumerate(ret) if isinstance(ret, list) else ret.items():
+        ctx = context
+        ctx.update({"item": elm})
+        print("+++: ", elm, ctx)
+        for k, v in obj.items():
+            out[_jq(k, ctx)] = _jq(v, ctx)
+
+    # pop unnecessary keys
+    # import pdb; pdb.set_trace()
+    out.pop("_dup", None)
+    for k in obj.keys():
+        print("+++: Delete duplicated object %s" % k)
+        data.pop(k, None)
+
+    return out
 
 def process(data, context, out={}):
     print("***: Input = %s" % data)
     print("***: Context = %s" % context)
+    print("***: Output = %s" % out)
 
     if isinstance(data, dict):
-        _process_include(data, context)
-
+        _process_include(data, context, out)
+        _process_dup(data, context, out)
         for k, v in data.items():
-            print("+++:", k, v)
-            if "_dup" in v:
-                # process jq value
-                value = v["_dup"]["with"]
-                if value.startswith("$"):
-                    ret = jq(value[1:]).transform(context)
-                else:
-                    ret = value
-
-                # loop
-                for elm in enumerate(ret) if isinstance(ret, list) else ret.items():
-                    print(elm)
-                
+            print(out)
+            print("+++: Iter", k, v)
             if isinstance(v, dict):
-                process(data[k], context, out)
+                out[k] = {}
+                process(data[k], context, out[k])
+                continue
+            out[k] = v
+
     return out 
             
 def main():
     context = DATA
-    for ex in EXAMPLES:
-        ret = process(yaml.load(ex["data"]), context)
+    for i, ex in enumerate(EXAMPLES):
+        if i not in (0,1,2,3,4,5,6):
+        # if i not in (3,4,):
+            continue
+        print("%03d: ##############################" % i)
+        ret = process(yaml.load(ex["data"]), context, {})
         print("***: Result = %s" % ret)
-        print("-->: Expect %s" % (ret == ex["result"]))
-        break
+        print("-->: Expect = %s" % (ex["result"]))
+        print("-->: %s" % (ret == ex["result"]))
+        print("")
 
 if __name__ == '__main__':
     main()
